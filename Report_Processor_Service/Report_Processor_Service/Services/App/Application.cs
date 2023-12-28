7fcdfc3874f0a0d8be8f97ea3bb30a0f282c15d3
salah aldin tanbour <salah.aldin.tanbour@outlook.com>
2023-12-29 00:20:45 +0200

    Register Services in IoC container

--- Report_Processor_Service/Report_Processor_Service/Services/App/Application.cs
+++ Report_Processor_Service/Report_Processor_Service/Services/App/Application.cs
@@ -1,10 +1,27 @@
-﻿namespace Report_Processor_Service.Services.App
+﻿using Report_Processor_Service.Models;
+using Report_Processor_Service.Services.MessageConsumer;
+using Report_Processor_Service.Services.MessageProcessor;
+
+namespace Report_Processor_Service.Services.App
 {
     public class Application : IApplication
     {
+        private readonly IMessageConsumer<EnviromentalReportMessage> _reportConsumer;
+        private readonly IMessageProcessor<EnviromentalReportMessage> _reportProcessor;
+
+        public Application(
+            IMessageConsumer<EnviromentalReportMessage> reportConsumer,
+            IMessageProcessor<EnviromentalReportMessage> reportProcessor)
+        {
+            _reportConsumer = reportConsumer ?? 
+                throw new ArgumentNullException(nameof(reportConsumer));
+            _reportProcessor = reportProcessor ?? 
+                throw new ArgumentNullException(nameof(reportProcessor));
+        }
         public void Run()
         {
-            
+            _reportConsumer.StartConsuming(_reportProcessor);
+            Console.ReadLine();
         }
     }
 }
---
+++ Report_Processor_Service/Report_Processor_Service/Services/MessageProcessor/EnviromentalReportProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report_Processor_Service.Services.MessageProcessor
+{
+    internal class EnviromentalReportProcessor
+    {
+    }
+}
